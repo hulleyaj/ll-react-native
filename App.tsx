c6fd91003168fd2db25f7f5d62f2700f2e09c039
Allen Hulley <hulleyaj@gmail.com>
2020-10-18 16:14:39 -0400

    redux counter

--- App.tsx
+++ App.tsx
@@ -1,10 +1,20 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
+import { Provider } from 'react-redux';
+import { createStore, combineReducers } from 'redux';
+import { counterReducer } from 'll-pkg';
 
 import useCachedResources from './hooks/useCachedResources';
 import useColorScheme from './hooks/useColorScheme';
 import Navigation from './navigation';
+
+const store = createStore(
+  combineReducers({
+    counterReducer
+  })
+);
+
 
 export default function App() {
   const isLoadingComplete = useCachedResources();
@@ -15,8 +25,10 @@ export default function App() {
   } else {
     return (
       <SafeAreaProvider>
-        <Navigation colorScheme={colorScheme} />
-        <StatusBar />
+        <Provider store={store}>
+          <Navigation colorScheme={colorScheme} />
+          <StatusBar />
+        </Provider>
       </SafeAreaProvider>
     );
   }
--- screens/TabOneScreen.tsx
+++ screens/TabOneScreen.tsx
@@ -1,20 +1,38 @@
 import * as React from 'react';
 import { StyleSheet } from 'react-native';
-import { Button } from 'll-pkg';
+import { connect } from 'react-redux';
+import { Button, decrementAction, incrementAction } from 'll-pkg';
 
 import EditScreenInfo from '../components/EditScreenInfo';
 import { Text, View } from '../components/Themed';
 
-export default function TabOneScreen() {
+function TabOneScreen({ decrementAction, count, incrementAction}) {
   return (
     <View style={styles.container}>
-      <Button text="lol" />
       <Text style={styles.title}>Tab One</Text>
+
+      <Button text="-" onClick={decrementAction} />
+      <Text>{count}</Text>
+      <Button text="+" onClick={incrementAction} />
       <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
       <EditScreenInfo path="/screens/TabOneScreen.js" />
     </View>
   );
 }
+
+const mapStateToProps = (state) => ({
+  count: state.counterReducer.count
+})
+
+const mapDispatchToProps = {
+  decrementAction,
+  incrementAction
+}
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(TabOneScreen);
 
 const styles = StyleSheet.create({
   container: {
